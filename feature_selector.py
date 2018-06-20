a79d4a4b9b3a124fafcccbb02f914470c936150f
Will Koehrsen <wjk68@case.edu>
2018-06-20 17:20:31 -0400

    Python class for identifying features to remove

---
+++ feature_selector.py
@@ -0,0 +1,407 @@
+import pandas as pd
+import numpy as np
+
+import lightgbm as lgb
+
+from sklearn.model_selection import KFold
+
+import gc
+
+from sklearn.exceptions import NotFittedError
+
+from itertools import chain
+
+class FeatureSelector():
+    """
+    Class for performing feature selection for machine learning or data preprocessing.
+    
+    Implements five different methods
+    
+        1. Remove columns with a missing percentage greater than a specified threshold
+        2. Remove columns with a single unique value
+        3. Remove collinear variables with a correlation greater than a specified correlation coefficient
+        4. Remove features with 0.0 feature importance from a gradient boosting machine (gbm)
+        5. Remove features that do not contribute to a specified cumulative feature importance from the gbm
+        
+    Attributes
+    --------
+    
+    record_missing : dataframe
+        Records the fraction of missing values for features with missing fraction above threshold
+    
+    record_single_unique : dataframe
+        Records the features that have a single unique value
+    
+    record_collinear : dataframe
+        Records the pairs of collinear variables with a correlation coefficient above the threshold
+    
+    record_zero_importance : dataframe
+        Records the zero importance features in the data according to the gbm
+    
+    record_low_importance : dataframe
+        Records the lowest importance features not needed to reach the threshold of cumulative importance according to the gbm
+    
+    feature_importances : dataframe
+        All the features importances from the gbm
+    
+    removal_ops : dict
+        Dictionary of removal operations and associated features for removal identified
+        
+    Notes
+    --------
+    
+        - All 5 operations can be run with the `identify_all` method.
+        - Calculating the feature importances requires labels (a supervised learning task) 
+          for training the gradient boosting machine
+        - For the feature importances, the dataframe is first one-hot encoded before training the gbm.
+    
+    """
+    
+    def __init__(self):
+        
+        # Dataframes recording information about features to remove
+        self.record_missing = None
+        self.record_single_unique = None
+        self.record_collinear = None
+        self.record_zero_importance = None
+        self.record_low_importance = None
+        
+        self.feature_importances = None
+        
+        # Dictionary to hold removal operations
+        self.removal_ops = {}
+        
+        
+        
+    def identify_missing(self, data, missing_threshold):
+        """Find the features with a fraction of missing values above `missing_threshold`"""
+        
+        self.missing_threshold = missing_threshold
+
+        # Calculate the fraction of missing in each column 
+        missing_series = data.isnull().sum() / data.shape[0]
+
+        # Find the columns with a missing percentage above the threshold
+        record_missing = pd.DataFrame(missing_series[missing_series > missing_threshold]).reset_index().rename(columns = {'index': 'feature', 0: 'missing_fraction'})
+
+        to_drop = list(record_missing['feature'])
+
+        self.record_missing = record_missing
+        self.removal_ops['missing'] = to_drop
+        
+        print('%d features with greater than %0.2f missing values.\n' % (len(self.removal_ops['missing']), self.missing_threshold))
+        
+    def identify_single_unique(self, data):
+        """Identifies features with only a single unique value. NaNs do not count as a unique value. """
+
+        # Calculate the unique counts in each column
+        unique_counts = data.nunique()
+
+        # Find the columns with only one unique count
+        record_single_unique = pd.DataFrame(unique_counts[unique_counts == 1]).reset_index().rename(columns = {'index': 'feature', 0: 'nunique'})
+
+        to_drop = list(record_single_unique['feature'])
+    
+        self.record_single_unique = record_single_unique
+        self.removal_ops['single_unique'] = to_drop
+        
+        print('%d features with a single unique value.\n' % len(self.removal_ops['single_unique']))
+    
+    def identify_collinear(self, data, correlation_threshold):
+        """
+        Finds collinear features based on the correlation coefficient between features. 
+        For each pair of features with a correlation coefficient greather than `correlation_threshold`,
+        only one of the pair is identified for removal. 
+
+        Using code adapted from: https://gist.github.com/Swarchal/e29a3a1113403710b6850590641f046c
+        
+        Parameters
+        --------
+
+        data : dataframe
+            Data observations in the rows and features in the columns
+
+        correlation_threshold : float between 0 and 1
+            Value of the Pearson correlation cofficient for identifying correlation features
+
+        """
+        
+        self.correlation_threshold = correlation_threshold
+
+        # Calculate the absolute value of the correlations between every column
+        corr_matrix = data.corr().abs()
+
+        # Extract the upper triangle of the correlation matrix
+        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+
+        # Select the features with correlations above the threshold
+        to_drop = [column for column in upper.columns if any(upper[column] > correlation_threshold)]
+
+        # Dataframe to hold correlated pairs
+        record_collinear = pd.DataFrame(columns = ['drop_feature', 'corr_feature', 'corr_value'])
+
+        # Iterate through the columns to drop
+        for column in to_drop:
+
+            # Find the correlated features
+            corr_features = list(upper_corrs.index[upper_corrs[column] > correlation_threshold])
+
+            # Find the correlated values
+            corr_values = list(upper_corrs[column][upper_corrs[column] > correlation_threshold])
+            drop_features = [column for _ in range(len(corr_features))]    
+
+            # Record the information (need a temp df for now)
+            temp_df = pd.DataFrame.from_dict({'drop_feature': drop_features,
+                                             'corr_feature': corr_features,
+                                             'corr_value': corr_values})
+
+            # Add to dataframe
+            record_collinear = record_collinear.append(temp_df, ignore_index = True)
+
+            
+        self.record_collinear = record_collinear
+        self.removal_ops['collinear'] = to_drop
+        
+        print('%d features with a correlation greater than %0.2f.\n' % (len(self.removal_ops['collinear']), self.correlation_threshold))
+
+    def identify_zero_importance(self, features, labels, eval_metric, task='classification', n_folds=5):
+        """
+        
+        Identify the features with zero importance according to a gradient boosting machine.
+        The gbm is trained with early stopping using a validation set to prevent overfitting. 
+        The feature importances are averaged over n_folds of cross validation to reduce variance. 
+        
+        Uses the LightGBM implementation (http://lightgbm.readthedocs.io/en/latest/index.html)
+
+        Parameters 
+        --------
+        features : dataframe
+            Data for training the model with observations in the rows
+            and features in the columns
+
+        labels : array, shape = (1, )
+            Array of labels for training the model. These can be either binary 
+            (if task is 'classification') or continuous (if task is 'regression')
+
+        eval_metric : string
+            Evaluation metric to use for the gradient boosting machine
+
+        task : string, default = 'classification'
+            The machine learning task, either 'classification' or 'regression'
+
+        n_folds : int, default = 5
+            Number of folds to use for cross validation
+            
+        Notes
+        --------
+        
+        - Features are one-hot encoded to handle the categorical variables before training.
+        - The gbm is not optimized for any particular task and might need some hyperparameter tuning
+        - Feature importances, including zero importance features, can change across runs
+
+        """
+
+        # One hot encoding
+        features = pd.get_dummies(features)
+
+        # Extract feature names
+        feature_names = list(features.columns)
+
+        # Convert to np array
+        features = np.array(features)
+        labels = np.array(labels).reshape((-1, ))
+
+        # Create the kfold object
+        k_fold = KFold(n_splits = n_folds, shuffle = True)
+
+        # Empty array for feature importances
+        feature_importance_values = np.zeros(len(feature_names))
+        
+        print('Training Gradient Boosting Model\n')
+        
+        # Iterate through each fold
+        for train_indices, valid_indices in k_fold.split(features):
+
+            train_features, train_labels = features[train_indices], labels[train_indices]
+            valid_features, valid_labels = features[valid_indices], labels[valid_indices]
+
+            if task == 'classification':
+                model = lgb.LGBMClassifier(n_estimators=10000, learning_rate = 0.05, verbose = -1)
+
+            elif task == 'regression':
+                model = lgb.LGBMRegressor(n_estimators=10000, learning_rate = 0.05, verbose = -1)
+
+            else:
+                raise ValueError('Task must be either "classification" or "regression"')
+
+            # Train the model with early stopping
+            model.fit(train_features, train_labels, eval_metric = eval_metric,
+                      eval_set = [(valid_features, valid_labels)],
+                      early_stopping_rounds = 100, verbose = -1)
+
+            # Record the feature importances
+            feature_importance_values += model.feature_importances_ / k_fold.n_splits
+
+            # Clean up memory
+            gc.enable()
+            del model, train_features, train_labels, valid_features, valid_labels
+            gc.collect()
+
+
+        feature_importances = pd.DataFrame({'feature': feature_names, 'importance': feature_importance_values})
+
+        # Sort features according to importance
+        feature_importances = feature_importances.sort_values('importance', ascending = False).reset_index(drop = True)
+
+        # Normalize the feature importances to add up to one
+        feature_importances['normalized_importance'] = feature_importances['importance'] / feature_importances['importance'].sum()
+        feature_importances['cumulative_importance'] = np.cumsum(feature_importances['normalized_importance'])
+
+        # Extract the features with zero importance
+        record_zero_importance = feature_importances[feature_importances['importance'] == 0.0]
+        
+        to_drop = list(record_zero_importance['feature'])
+
+        self.feature_importances = feature_importances
+        self.record_zero_importance = record_zero_importance
+        self.removal_ops['zero_importance'] = to_drop
+        
+        print('\n%d features with zero importance.\n' % len(self.removal_ops['zero_importance']))
+    
+    def identify_low_importance(self, cumulative_importance):
+        """
+        Finds the lowest importance features not needed to account for `cumulative_importance` 
+        of the feature importance from the gradient boosting machine. As an example, if cumulative
+        importance is set to 0.95, this will retain only the most important features needed to 
+        reach 95% of the total feature importance. The identified features are those not needed.
+
+        Parameters
+        --------
+        cumulative_importance : float between 0 and 1
+            The fraction of cumulative importance to account for 
+
+        """
+
+        self.cumulative_importance = cumulative_importance
+        
+        # The feature importances need to be calculated before running
+        if self.feature_importances is None:
+            raise NotFittedError('Feature importances have not yet been determined. Call the `identify_zero_importance` method` first.')
+            
+        # Make sure most important features are on top
+        self.feature_importances = self.feature_importances.sort_values('cumulative_importance')
+
+        # Identify the features not needed to reach the cumulative_importance
+        record_low_importance = self.feature_importances[self.feature_importances['cumulative_importance'] > cumulative_importance]
+
+        to_drop = list(record_low_importance['feature'])
+
+        self.record_low_importance = record_low_importance
+        self.removal_ops['low_importance'] = to_drop
+    
+        print('%d features that do not contribute to cumulative importance of %0.2f.\n' % (len(self.removal_ops['low_importance']), self.cumulative_importance))
+        
+    def identify_all(self, features, labels, selection_params):
+        """
+        Use all five of the methods to identify features to remove.
+        
+        Parameters
+        --------
+        
+        features : dataframe
+            Data for training the model with observations in the rows
+            and features in the columns
+
+        labels : array, shape = (1, )
+            Array of labels for training the model. These can be either binary 
+            (if task is 'classification') or continuous (if task is 'regression')
+            
+        selection_params : dict
+           Parameters to use in the five feature selection methhods.
+           Params must contain the keys ['missing_threshold', 'correlation_threshold', 'eval_metric', 'task', 'cumulative_importance']
+        
+        """
+        
+        # Check for all required parameters
+        for param in ['missing_threshold', 'correlation_threshold', 'eval_metric', 'task', 'cumulative_importance']:
+            if param not in selection_params.keys():
+                raise ValueError('%s is a required parameter for this method' % param)
+        
+        # Implement each of the five methods
+        self.identify_missing(features, selection_params['missing_threshold'])
+        self.identify_single_unique(features)
+        self.identify_collinear(features, selection_params['correlation_threshold'])
+        self.identify_zero_importance(features, labels, selection_params['eval_metric'], selection_params['task'])
+        self.identify_low_importance(selection_params['cumulative_importance'])
+        
+        # Find the number of features identified to drop
+        self.n_identified = len(set(list(chain(*list(self.removal_ops.values())))))
+        print('%d total features out of %d identified for removal.\n' % (self.n_identified, pd.get_dummies(features).shape[1]))
+        
+    def check_identified(self):
+        """Check the identified features before removal. Returns a set of the unique features identified."""
+        
+        all_identified = set(list(chain(*list(self.removal_ops.values()))))
+        print('%d features identified for removal' % len(all_identified))
+        
+        return all_identified
+        
+    
+    def remove(self, data, methods):
+        """
+        Remove the features from the data according to the specified methods.
+        
+        Parameters
+        --------
+            data : dataframe
+                Dataframe with features to remove
+            methods : 'all' or list of methods
+                If methods == 'all', any methods that have identified features will be used
+                Otherwise, only the specified methods will be used.
+                Can be one of ['missing', 'single_unique', 'collinear', 'zero_importance', 'low_importance']
+                
+        Return
+        --------
+            data : dataframe
+                Dataframe with identified features removed
+                
+        
+        Notes 
+        --------
+            - This first one-hot encodes the categorical variables in accordance with the gradient boosting machine.
+            - Check the features that will be removed before transforming data!
+        
+        """
+        
+        
+        features_to_drop = []
+        
+        data = pd.get_dummies(data)
+        
+        if methods == 'all':
+            
+            print('{} methods have been run'.format(list(self.removal_ops.keys())))
+            
+            # Find the unique features to drop
+            features_to_drop = set(list(chain(*list(self.removal_ops.values()))))
+            
+        else:
+            # Iterate through the specified methods
+            for method in methods:
+                # Check to make sure the method has been run
+                if method not in self.removal_ops.keys():
+                    raise NotFittedError('%s method has not been run' % method)
+                    
+                # Append the features identified for removal
+                else:
+                    features_to_drop.append(self.removal_ops[method])
+        
+            # Find the unique features to drop
+            features_to_drop = set(list(chain(*features_to_drop)))
+            
+        # Remove the features and return the data
+        data = data.drop(columns = features_to_drop)
+        self.removed_features = features_to_drop
+        
+        print('Removed %d features' % len(features_to_drop))
+        return data
