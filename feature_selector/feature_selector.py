d12ac7319a1abc909f6c857a65e35d9508d7da3c
Alan Melo Clappis <6784627+clappis@users.noreply.github.com>
2019-04-06 23:21:00 -0300

    Add stratify to split

--- feature_selector/feature_selector.py
+++ feature_selector/feature_selector.py
@@ -303,7 +303,7 @@ class FeatureSelector():
             # If training using early stopping need a validation set
             if early_stopping:
                 
-                train_features, valid_features, train_labels, valid_labels = train_test_split(features, labels, test_size = 0.15)
+                train_features, valid_features, train_labels, valid_labels = train_test_split(features, labels, test_size = 0.15, stratify=labels)
 
                 # Train the model with early stopping
                 model.fit(train_features, train_labels, eval_metric = eval_metric,
